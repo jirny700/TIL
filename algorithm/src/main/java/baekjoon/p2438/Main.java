219a2cc5a56038598bad5971b80b95ad8887f828
jirny700 <jirny700@gmail.com>
2022-03-16 22:05:19 +0900

    백준포문

---
+++ algorithm/src/main/java/baekjoon/p2438/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.p2438;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.nextLine();
+        String[] split = next.split(" ");
+        int n = Integer.parseInt(split[0]);
+        solution(n);
+    }
+
+    private static void solution(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

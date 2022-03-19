842c3d7f8e3e7fa4a907a5a5f67adacf554ffb09
jirny700 <jirny700@gmail.com>
2022-03-19 14:40:10 +0900

    1일차 숙제

---
+++ totolecture/src/main/java/lecture220317/intro/Intro.java
@@ -0,0 +1,10 @@
+package lecture220317.intro;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("jirny");
+        System.out.println("female");
+        System.out.println("jirnyy700@gmail.com");
+    }
+}
+

a6a024ceb81037abbc1288c5901fd62bb7fc7cee
jirny700 <jirny700@gmail.com>
2022-03-20 00:17:21 +0900

    2일차 숙제

---
+++ totolecture/src/main/java/lecture220319/star/Star.java
@@ -0,0 +1,11 @@
+package lecture220319.star;
+
+public class Star {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}

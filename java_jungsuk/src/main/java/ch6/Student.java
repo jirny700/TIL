ebb263cca96232f810ce0f90d30d1ece7bdbbb81
jirny700 <jirny700@gmail.com>
2022-03-23 21:27:26 +0900

    자바의 정석 연습문제

---
+++ java_jungsuk/src/main/java/ch6/Student.java
@@ -0,0 +1,35 @@
+package ch6;
+
+public class Student {
+    String name;
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+
+    public Student(String name,int ban,int no,int kor,int eng,int math){
+        this.name=name;
+        this.ban=ban;
+        this.no=no;
+        this.kor=kor;
+        this.eng=eng;
+        this.math=math;
+    }
+    public Student(){
+
+    }
+
+    int getTotal() {
+        return (kor + eng + math);
+    }
+
+    float getAverage() {
+        return (kor + eng + math) / 3;
+    }
+    String info(){
+        return (name+","+ban+","+no+","+kor+","+eng+","+math);
+    }
+
+
+}

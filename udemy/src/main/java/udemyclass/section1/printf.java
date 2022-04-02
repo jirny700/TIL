efb0503cca250f25f2c708b0bb89ce0fbce2c28f
jirny700 <jirny700@gmail.com>
2022-04-02 21:35:42 +0900

    유데미 복습

---
+++ udemy/src/main/java/udemyclass/section1/printf.java
@@ -0,0 +1,7 @@
+package udemyclass.section1;
+
+public class printf {
+    public static void main(String[] args) {
+        System.out.printf("%d + %d + %d = %d", 5, 6, 7, 5 + 6 + 7).println();
+    }
+}

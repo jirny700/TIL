a1d6c2dfc26bff7117d32b28aa9be1620d5085b2
jirny700 <jirny700@gmail.com>
2022-04-06 23:24:28 +0900

    유데미 연습

---
+++ udemy/src/main/java/udemyclass/section6/MultiplicationTableRunner.java
@@ -0,0 +1,8 @@
+package udemyclass.section6;
+
+public class MultiplicationTableRunner {
+    public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
+        table.print(5);
+    }
+}

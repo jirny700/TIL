bbd68d19507a87328f2a3bc117974e8f1119e6cf
jirny700 <jirny700@gmail.com>
2022-04-08 23:39:57 +0900

    유데미 연습

--- udemy/src/main/java/udemyclass/section8/MotorBike.java
+++ udemy/src/main/java/udemyclass/section8/MotorBike.java
@@ -2,8 +2,13 @@ package udemyclass.section8;
 
 public class MotorBike {
 
-    int speed;
-
+    private int speed;
+    void setSpeed(int speed){
+        this.speed = speed;
+    }
+    int getSpeed(){
+        return this.speed;
+    }
     void start(){
         System.out.println("bike start!!");
     }
--- udemy/src/main/java/udemyclass/section8/MotorBikeRunner.java
+++ udemy/src/main/java/udemyclass/section8/MotorBikeRunner.java
@@ -7,7 +7,14 @@ public class MotorBikeRunner {
 
         ducati.start();
         honda.start();
-        ducati.speed = 100;
-        honda.speed = 0;
+       // ducati.speed = 100; 사용 불가함. 모터바이크 클래스의 스피드가 캡슐화(private)되어 있기 때문에.
+       // honda.speed = 0;
+
+        ducati.setSpeed(100);
+        System.out.println(ducati.getSpeed());
+
+        honda.setSpeed(80);
+        System.out.println(honda.getSpeed());
+
     }
 }

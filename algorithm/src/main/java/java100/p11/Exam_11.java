ced17d92e9433daec000366ac32bcc6edd3ceed4
jirny700 <jirny700@gmail.com>
2022-04-18 22:30:22 +0900

    자바100제

--- algorithm/src/main/java/java100/p11/Exam_11.java
+++ algorithm/src/main/java/java100/p11/Exam_11.java
@@ -1,2 +1,27 @@
-package java100.p11;public class Exam_11 {
+package java100.p11;
+
+public class Exam_11 {
+    public static void main(String[] args) {
+        int a; //정수가 저장될 변수 이름을 a로 만들어라.
+        int b;
+        int sum;
+
+        a = 3;
+        b = 7;
+        sum = a + b;
+
+        System.out.println(sum);
+
+        sum = a + b + a;
+        System.out.println(sum);
+    }
 }
+
+//    1.변수란 무엇인가?
+//    -->바구니. 데이터를 저장하는 메모리 공간이다. 변하는 수이다.
+//    2.변수의 선언이란?
+//    -->변수를 사용하기 위해서는 먼저 변수의 탑입에 맞는 선언을 해줘야 한다.
+//    -->정수형의 변수를 사용하고자 한다면 먼저 정수형 타입의 자료형을 선언해줘야 함.
+//    3.변수의 용도?
+//    -->저장. 어떤 연산을 수행할 떄 값들을 저장해놓아야 하는데 그떄 변수를 활용한다.
+//

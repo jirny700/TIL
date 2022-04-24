b1f9b9733cf65faee39bcf05da2dd6f65e5bb07a
jirny700 <jirny700@gmail.com>
2022-04-24 22:54:33 +0900

    자바100제

--- algorithm/src/main/java/java100/p17/Exam_17.java
+++ algorithm/src/main/java/java100/p17/Exam_17.java
@@ -1,2 +1,27 @@
-package java100.p17;public class Exam_17 {
+package java100.p17;
+
+public class Exam_17 {
+    public static void main(String[] args) {
+       //기본형타입->8개: 변수 선언과 동시에 값을 입력
+        byte b = 10;
+        short s=32767;
+        int i=2147483647;
+        long l= 700000000L;
+        float f=9.8f;
+        double d=3.14;
+        char c='a';
+        boolean bl=true;
+
+        //print,println
+        System.out.print("안녕하세요");
+        System.out.print("모두 안녕");
+        System.out.println("홍길동입니다. 나는 학생입니다");
+
+        //printf->지시자를 사용
+        //%d(정수),%f(소숫점형식),%c(문자),%s(문자열),%b(불린형),%n(줄바꿈),%o(8진수),%x(16진수)
+        System.out.printf("나는 %d살의 대학생입니다. %d,%d,%d,%.1f,%.2f",b,s,i,l,f,d);
+        System.out.println();
+        System.out.printf("10진수 10은 8진수로는 %o이고, 16진수로는 %x이다",b,b);
+
+    }
 }
--- algorithm/src/main/java/java100/p17/Exam_17_1.java
+++ algorithm/src/main/java/java100/p17/Exam_17_1.java
@@ -1,2 +1,26 @@
-package java100.p17;public class Exam_17_1 {
+package java100.p17;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Exam_17_1 {
+    public static void main(String[] args) {
+
+        //정수선언
+        int a = 12345;
+        String str = "12345";
+        System.out.println(str.length()); //5 //문자열의 자릿수는 length를 통해 출력함
+        //System.out.println(a.length());//에러남.
+
+        //정수->문자열로 변환->자릿수 구하기
+        String stra = String.valueOf(a);
+        System.out.println(stra.length());//5
+
+        //문자열+정수형=?
+        System.out.println(12345+1);//12346
+        System.out.println(str+1);//123451
+
+        //문자열->정수로변환
+        int b=Integer.valueOf(str);
+        System.out.println(b+1);//12346
+    }
 }

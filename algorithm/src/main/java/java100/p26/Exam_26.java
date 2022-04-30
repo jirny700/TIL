a45822116141c9c81ff5e30ba27d6596fcc7abae
jirny700 <jirny700@gmail.com>
2022-04-30 16:54:03 +0900

    자바100제 26분

---
+++ algorithm/src/main/java/java100/p26/Exam_26.java
@@ -0,0 +1,10 @@
+package java100.p26;
+
+public class Exam_26 {
+    public static void main(String[] args) {
+        //for 반복문-->A(65)~Z(90)
+        for (int i = 65; i <= 90 ; i++) {
+            System.out.println((char)i);
+        }
+    }
+}

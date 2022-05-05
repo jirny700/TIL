d6a7e40d0546876a968ecab098ffdd6bb0d9eff6
jirny700 <jirny700@gmail.com>
2022-05-05 23:03:10 +0900

    자바100제 git commit -m자바100제 git push git status git status

--- algorithm/src/main/java/java100/p35/Exam_35.java
+++ algorithm/src/main/java/java100/p35/Exam_35.java
@@ -5,7 +5,7 @@ public class Exam_35 {
         //[1]: String 1차원 배열-->3개의 문자열을 담을 수 있는 배열 생성하기
         //String 타입의 참조변수 3개를 저장하기 위한 공간 생성-->이때 참조형 변수 각 요소는 기본값-->String은 null로 초기화.
         String[] province = new String[3];
-        System.out.println(province[0]);//-null
+        System.out.println(province[0]);//-null값 출력
         System.out.println(province[1]);
         System.out.println(province[2]);
 
@@ -31,5 +31,27 @@ public class Exam_35 {
         for (String aaa : flower) {
             System.out.println(aaa);
         }
+
+        //[6]String 2차원 배열 만들기-->2*3
+        //아시아-->동북아시아(한국,중국,일본),동남아시아(태국,필리핀,베트남)
+        String [][] asiaNations = {{"한국","중국","일본"},{"태국","베트남","필리핀"}};
+        System.out.println(asiaNations[0]);//참조값이 출력됨
+        System.out.println(asiaNations[0][1]);//중국: 실제 요소의 값 출력
+
+        System.out.println(asiaNations.length);//2
+        System.out.println(asiaNations[0].length);//3
+        System.out.println(asiaNations[1].length);//3
+
+        //[7]: 반복문을 사용한 String 2차원 배열 값 출력
+        for (int i = 0; i < asiaNations.length; i++) {
+            for (int j = 0; j < asiaNations[i].length; j++) {
+                System.out.println(asiaNations[i][j]);
+            }
+        }
+
+        //[8]charAt()메서드 사용-->해당 인덱스에 있는 값을 반환-->단어를 char배열에 한글자씩 저장할 수 있음.
+        String[] strAr={"hong","kim","park"};
+        System.out.println(strAr[0].charAt(2 ));
+
     }
 }

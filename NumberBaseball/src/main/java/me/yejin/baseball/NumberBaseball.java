e2a5f7081992ab33b083f2deea58ac0dc4ac95e8
jirny700 <jirny700@gmail.com>
2022-06-14 22:19:16 +0900

    숫자 4개 입력받기

--- NumberBaseball/src/main/java/me/yejin/baseball/NumberBaseball.java
+++ NumberBaseball/src/main/java/me/yejin/baseball/NumberBaseball.java
@@ -1,14 +1,11 @@
 package me.yejin.baseball;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class NumberBaseball {
     public static void main(String[] args) {
 
-//        int oneBase = (int)Math.random()*10;
-//        int twoBase = (int)Math.random()*10;
-//        int threeBase = (int)Math.random()*10;
-//        int fourBase = (int)Math.random()*10;
         Random random = new Random();
         int[] baseballAnswer = new int[4];
         for (int i = 0; i < 4; i++) {
@@ -24,6 +21,20 @@ public class NumberBaseball {
             System.out.println(baseballAnswer[i]);
         }
 
+        Scanner userEnterNumber = new Scanner(System.in);
+
+           int oneBase;
+           int twoBase;
+           int threeBase;
+           int fourBase;
+
+        System.out.println("숫자 4개를 입력하세요(숫자 하나 입력 후 띄어쓰기 하세요).");
+        oneBase = userEnterNumber.nextInt();
+        twoBase = userEnterNumber.nextInt();
+        threeBase = userEnterNumber.nextInt();
+        fourBase = userEnterNumber.nextInt();
+
+        System.out.printf("당신이 입력한 숫자는 %d, %d, %d , %d 입니다.", oneBase,twoBase,threeBase,fourBase);
 
 
     }
